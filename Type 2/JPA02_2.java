8d7e6b6ea2f649435cb4e2f80a5585d1c1346327
qwerx64815 <36224919+qwerx64815@users.noreply.github.com>
2021-03-22 17:22:14 +0800

    Update JPA02_2.java

--- Type 2/JPA02_2.java
+++ Type 2/JPA02_2.java
@@ -6,13 +6,16 @@ class JPA02_2 {
 		Scanner sc = new Scanner(System.in);
 		for(int i = 0; i < 2; i++) {
 			out.println("Input: ");
-			compareNum(sc.nextInt(), sc.nextInt());
+			try { compareNum(sc.nextInt(), sc.nextInt()); }
+			catch(Exception ex) { out.println("Input error!"); }
 		}
 	}
 	
 	private static void compareNum(int n1, int n2) {
-		if(n1 > n2) out.printf("%d is larger than %d\n", n1, n2);
-		else if(n1 < n2) out.printf("%d is larger than %d\n", n2, n1);
+		if(n1 != n2) 
+			out.printf("%d is larger than %d\n", 
+				Math.max(n1, n2), 
+				Math.min(n1, n2));
 		else out.printf("%d equals %d", n1, n2);
 	}
 }
