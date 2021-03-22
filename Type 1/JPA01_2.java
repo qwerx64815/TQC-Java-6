0acb87969c0bf52d3e6faedbd6bdd94a251f2842
qwerx64815 <36224919+qwerx64815@users.noreply.github.com>
2021-03-22 17:41:04 +0800

    Update JPA01_2.java

--- Type 1/JPA01_2.java
+++ Type 1/JPA01_2.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 class JPA01_2 {
 	public static void main(String[] args) {
 		out.print("Please input: ");
-		double p = new Scanner(System.in).nextDouble();
+		double p = 0.0;
+		try { p = new Scanner(System.in).nextDouble(); }
+		catch(Exception ex) { out.println("Input error!"); }
 		out.printf("%f kg = %f ponds", p, p*2.20462);
 	}
 }
