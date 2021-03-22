956a1aa3c98ec9d7b907770b547fd80d57f58322
qwerx64815 <36224919+qwerx64815@users.noreply.github.com>
2021-03-22 17:49:35 +0800

    Update JPA01_5.java

--- Type 1/JPA01_5.java
+++ Type 1/JPA01_5.java
@@ -2,26 +2,34 @@ import static java.lang.System.out;
 import java.util.Scanner;
 
 class JPA01_5 {
+	static Scanner sc = new Scanner(System.in);
+	static int coinSum = 0;
+	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
 		out.print("請輸入您的姓名： ");
-		String name = sc.next();
+		try { 
+			String name = sc.next();
+			out.printf("Hi, %s, 請輸入您的銅板個數。\n", name);
+		} catch(Exception ex) { out.println("Input name error!"); }
 		
-		int coinSum = 0;
-		out.printf("Hi, %s, 請輸入您的銅板個數。\n", name);
 		out.print("請輸入 1 元數量： ");
-		coinSum = sc.nextInt();
+		inputCoin(1);
 		out.print("請輸入 5 元數量： ");
-		coinSum += sc.nextInt() * 5;
+		inputCoin(5);
 		out.print("請輸入 10 元數量： ");
-		coinSum += sc.nextInt() * 10;
+		inputCoin(10);
 		out.print("請輸入 50 元數量： ");
-		coinSum += sc.nextInt() * 50;
+		inputCoin(50);
+		
 		out.printf("您的錢總共有： %d 千 %d 百 %d 十 %d 元", 
-				coinSum/1000, 
-				(coinSum%1000)/100, 
-				(coinSum%100)/10, 
-				coinSum%10);
+				coinSum / 1000, 
+				(coinSum % 1000) / 100, 
+				(coinSum % 100) / 10, 
+				coinSum % 10);
+	}
+	
+	private static void inputCoin(int times) {
+		try { coinSum += sc.nextInt() * times; }
+		catch(Exception ex) { out.println("Input coin error!"); }
 	}
 }
